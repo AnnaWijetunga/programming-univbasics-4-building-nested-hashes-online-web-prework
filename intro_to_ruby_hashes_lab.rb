e98eec286be9e773969e69e1c3b3f8621c11a6cb
Anna Wijetunga <annawijetunga@gmail.com>
2019-10-31 15:18:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,18 +12,13 @@ def monopoly_with_second_tier
 	}
 end
 
-#the monopoly_with_third_tier method builds on the Hash returned by monopoly_with_second_tier, but updates the Hash pointed at by :railroads such that the Hash
-   # has a key called :rent_in_dollars (FAILED - 1)
-    
-   # but updates the Hash pointed at by :railroads such that the Hash has a key called :rent_in_dollars
-     #Failure/Error: expect(monopoly_with_third_tier[:railroads].has_key?(:rent_in_dollars)).to(be_truthy, ":railroads should point to a Hash with key rent_in_dollars")
-     #  :railroads should point to a Hash with key rent_in_dollars
-
 def monopoly_with_third_tier
   monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	    :rent_in_dollars => {}
+	    :rent_in_dollars => {
+	      
+	    }
 	  }
 	}
 end
