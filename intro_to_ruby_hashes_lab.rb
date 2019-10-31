8bccd69deabdd57d7dedfef12d2d8099ed8b88ed
Anna Wijetunga <annawijetunga@gmail.com>
2019-10-31 15:11:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,30 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+	monopoly = {
+	  :railroads => {}
+	}
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+ 	monopoly = {
+	  :railroads => {
+	    :pieces => 4
+	  }
+	}
 end
 
+#the monopoly_with_third_tier method builds on the Hash returned by monopoly_with_second_tier, but updates the Hash pointed at by :railroads such that the Hash
+    has a key called :rent_in_dollars (FAILED - 1)
+    
+    #1) We can create complex, nested Hashes by implementing four methods: the monopoly_with_third_tier method builds on the Hash returned by monopoly_with_second_tier, but updates the Hash pointed at by :railroads such that the Hash has a key called :rent_in_dollars
+     #Failure/Error: expect(monopoly_with_third_tier[:railroads].has_key?(:rent_in_dollars)).to(be_truthy, ":railroads should point to a Hash with key rent_in_dollars")
+     #  :railroads should point to a Hash with key rent_in_dollars
+
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  monopoly = {
+	  :railroads => {
+	    :pieces => 4
+	  }
+	}
 end
 
 def monopoly_with_fourth_tier
